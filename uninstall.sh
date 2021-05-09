155643e24ea33d2e8bb45bdb5492fb9a8e616196
Chintan Patel <chynten@gmail.com>
2021-05-09 23:35:24 +0100

    adding log

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+echo "---> stopping all conatainers"
 sudo docker stop $(sudo docker ps -aq)
+echo "---> removing all conatainers"
 sudo docker rm $(sudo docker ps -a)
-
+echo "---> removing all conatainer images"
 sudo docker rmi $(sudo docker images -a)
 
+echo "---> removing istio tmp folder"
 sudo rm -r infra/istio/tmp
+echo "---> uninstalling k3s"
 sudo /usr/local/bin/k3s-uninstall.sh
