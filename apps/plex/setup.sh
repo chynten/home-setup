932f6be66dea6781ed0aac1bdc903bfda5c1ce44
Chintan Patel <chynten@gmail.com>
2021-05-11 18:53:04 +0100

    Updating setup file

--- apps/plex/setup.sh
+++ apps/plex/setup.sh
@@ -2,14 +2,14 @@
 
 . ../../VERSION.sh
 
-read -p "Plex Domain:" PLEX_DOMAIN
-read -p "Plex Config Directory" PLEX_CONFIG_DIR
-read -p "Plex Media Directory" PLEX_MEDIA_DIR
-read -p "Claim Refenrece (https://www.plex.tv/claim/):" PLEX_CLAIM_REF
+read -p "Plex Domain: " PLEX_DOMAIN
+read -p "Plex Config Directory: " PLEX_CONFIG_DIR
+read -p "Plex Media Directory: " PLEX_MEDIA_DIR
+read -p "Claim Refenrece (https://www.plex.tv/claim/): " PLEX_CLAIM_REF
 
-export $PLEX_DOMAIN
-export $PLEX_CONFIG_DIR
-export $PLEX_MEDIA_DIR
+export PLEX_DOMAIN=$PLEX_DOMAIN
+export PLEX_CONFIG_DIR=$PLEX_CONFIG_DIR
+export PLEX_MEDIA_DIR=$PLEX_MEDIA_DIR
 export PLEX_CLAIM_REF=`echo -n $PLEX_CLAIM_REF | base64 -w 0`
 
 sudo mkdir -p $PLEX_CONFIG_DIR
