b54ba414eeabbbd41b9d646d56496bd3922e761f
Chintan Patel <chynten@gmail.com>
2021-05-11 19:37:05 +0100

    updating cluster issuer

--- infra/certificate/setup.sh
+++ infra/certificate/setup.sh
@@ -5,11 +5,9 @@
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/$CERT_BOT_VERSION/cert-manager.crds.yaml
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/$CERT_BOT_VERSION/cert-manager.yaml
 
-read -p "Cloudflair API Key: " CLOUDFLARE_API_KEY
 read -p "Cloudflair API Token: " CLOUDFLARE_API_TOKEN
 read -p "Cloudflair email: " CLOUDFLARE_EMAIL
 
-export CLOUDFLARE_API_KEY=`echo -n $CLOUDFLARE_API_KEY | base64 -w 0`
 export CLOUDFLARE_API_TOKEN=`echo -n $CLOUDFLARE_API_TOKEN | base64 -w 0`
 export CLOUDFLARE_EMAIL=$CLOUDFLARE_EMAIL
 
