8a46fb91c5dc61aea398fb23bdf599a3793da82f
Chintan Patel <chynten@gmail.com>
2021-05-12 14:28:59 +0100

    envoy monitoring

--- infra/monitoring/setup.sh
+++ infra/monitoring/setup.sh
@@ -7,4 +7,6 @@ export DATADOG_API_KEY=$datadog_api_key
 helm repo add datadog https://helm.datadoghq.com
 helm repo add stable https://charts.helm.sh/stable
 helm repo update
-helm upgrade datadog --install --namespace monitoring --create-namespace -f datadog-values.yaml --set datadog.site='datadoghq.eu' --set datadog.apiKey=$DATADOG_API_KEY datadog/datadog 
+kubectl create namespace monitoring
+kubectl label namespace monitoring istio-injection=enabled
+helm upgrade datadog --install --namespace monitoring -f datadog-values.yaml --set datadog.site='datadoghq.eu' --set datadog.apiKey=$DATADOG_API_KEY datadog/datadog 
