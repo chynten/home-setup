4ebc96ca6ff9d6b767f5c1845a8e67984c780448
Chintan Patel <chynten@gmail.com>
2021-05-18 15:39:02 +0100

    DB viewer

--- apps/db-client/setup.sh
+++ apps/db-client/setup.sh
@@ -12,4 +12,13 @@ sudo chmod 777 $CLOUDBEAVER_CONFIG_DIR
 export CLOUDBEAVER_CONFIG_DIR=$CLOUDBEAVER_CONFIG_DIR
 export CLOUDBEAVER_DOMAIN=$CLOUDBEAVER_DOMAIN
 
-envsubst < mysql-Namespace.yaml | kubectl apply -f -
+envsubst < CloudBeaver-Certificate.yaml | kubectl apply -f -
+envsubst < CloudBeaver-Gateway.yaml | kubectl apply -f -
+
+envsubst < CloudBeaver-PersistentVolume.yaml | kubectl apply -f -
+envsubst < CloudBeaver-PersistentVolumeClaim.yaml | kubectl apply -f -
+
+envsubst < CloudBeaver-Deployment.yaml | kubectl apply -f -
+
+envsubst < CloudBeaver-Service.yaml | kubectl apply -f -
+envsubst < CloudBeaver-VirtualService.yaml | kubectl apply -f -
