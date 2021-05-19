0d67d71ee7ce0b01c95cf8a06edd65a98726cf34
Chintan Patel <chynten@gmail.com>
2021-05-19 15:53:46 +0100

    keycloak deployment reverse proxy support

--- apps/sso/setup.sh
+++ apps/sso/setup.sh
@@ -3,19 +3,13 @@
 
 echo "---> setting up Keycloak"
 read -p "Keycloak domain name: " KEYCLOAK_DOMAIN
-read -p "keycloak database Password: " KEYCLOAK_DB_PASSWORD
 read -p "Keycloak Admin password: " KEYCLOAK_PASSWORD
+read -p "Keycloak Data dir: " KEYCLOAK_DATA_DIR
 
 export KEYCLOAK_DOMAIN=$KEYCLOAK_DOMAIN
-export KEYCLOAK_DB_PASSWORD=`echo -n $KEYCLOAK_DB_PASSWORD | base64 -w 0`
 export KEYCLOAK_PASSWORD=`echo -n $KEYCLOAK_PASSWORD | base64 -w 0`
+export KEYCLOAK_DATA_DIR=$KEYCLOAK_DATA_DIR
 
-kubectl exec --stdin --tty mysql-statefuleset-0 -n mysql -- mysql -u root -p$ROOT_PASSWORD <<EOF
-    CREATE DATABASE IF NOT EXISTS keycloak;
-    CREATE USER IF NOT EXISTS 'keycloak'@'%' IDENTIFIED WITH mysql_native_password by '${$KEYCLOAK_DB_PASSWORD}';
-    GRANT ALL privileges ON keycloak.* TO 'keycloak'@'%';
-    FLUSH PRIVILEGES;
-EOF
 
 envsubst < keycloak-Namespace.yaml | kubectl apply -f -
 
@@ -23,6 +17,9 @@ envsubst < keycloak-Certificate.yaml | kubectl apply -f -
 envsubst < keycloak-Gateway.yaml | kubectl apply -f -
 
 envsubst < keycloak-Secret.yaml | kubectl apply -f -
+
+envsubst < plex-PersistentVolume.yaml | kubectl apply -f -
+envsubst < plex-PresistentVolumeClaim.yaml | kubectl apply -f -
 
 envsubst < keycloak-Deployment.yaml | kubectl apply -f -
 
