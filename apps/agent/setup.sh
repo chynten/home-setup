675a6b87addbf2b30871ac84646330bfc7285282
Chintan Patel <chynten@gmail.com>
2021-05-25 22:04:04 +0100

    Agent setup with pvc

--- apps/agent/setup.sh
+++ apps/agent/setup.sh
@@ -5,6 +5,7 @@
 read -p "Huginn Domain: " HUGINN_DOMAIN
 read -p "MySQL Root Password: " ROOT_PASSWORD
 read -p "Huginn DB Password: " HUGINN_DB_PASSWORD
+read -p "Huginn data dir: " HUGINN_DATA_DIR
 read -p "Gmail email: " GMAIL_EMAIL
 read -p "Gmail SMTP Password: " SMTP_PASSWORD
 
@@ -15,6 +16,7 @@ kubectl exec --stdin --tty mysql-statefuleset-0 -n mysql -- mysql -u root -p$ROO
     FLUSH PRIVILEGES;
 EOF
 
+export HUGINN_DATA_DIR=$HUGINN_DATA_DIR
 export HUGINN_DOMAIN=$HUGINN_DOMAIN
 export HUGINN_DB_PASSWORD=`echo -n $HUGINN_DB_PASSWORD | base64 -w 0`
 export GMAIL_EMAIL=`echo -n $GMAIL_EMAIL | base64 -w 0`
