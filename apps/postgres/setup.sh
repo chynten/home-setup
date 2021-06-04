add1f1de154eae6e3eb847f57d7cdaab90b02849
Chintan Patel <chynten@gmail.com>
2021-06-04 13:52:39 +0100

    postgres update

--- apps/postgres/setup.sh
+++ apps/postgres/setup.sh
@@ -6,7 +6,7 @@ read -p "Postgress Admin Password: " POSTGRES_PASSWORD
 read -p "Postgres Data Dir: " POSTGRES_DATA_DIR
 
 export POSTGRES_PASSWORD=`echo -n $POSTGRES_PASSWORD | base64 -w 0`
-export POSTGRES_DATA_DIR=POSTGRES_DATA_DIR
+export POSTGRES_DATA_DIR=$POSTGRES_DATA_DIR
 sudo mkdir -p ${POSTGRES_DATA_DIR}
 
 envsubst < postgres-Namespace.yaml | kubectl apply -f -
